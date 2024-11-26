5d3fcd448bbef31834794bca293a99b91549a6b1
Mariusz Milejko <mariusz@milejko.pl>
2024-11-26 12:35:24 +0100

    env populating fix

--- src/App/AppGetEnvironment.php
+++ src/App/AppGetEnvironment.php
@@ -17,16 +17,27 @@ class AppGetEnvironment
 {
     public function __invoke(): array
     {
+        //.env file path definitions
         $dotEnvFilePath = BASE_PATH . '/.env';
         $dotEnvLocalFilePath = BASE_PATH . '/.env.local';
+        //.env values
         $dotEnvValues = file_exists($dotEnvFilePath) ? parse_ini_file($dotEnvFilePath, true) : [];
         $dotEnvLocalValues = file_exists($dotEnvLocalFilePath) ? parse_ini_file($dotEnvLocalFilePath, true) : [];
+        //merging .env with .env.local
         $dotEnvVars = array_merge(
             $dotEnvValues ? $dotEnvValues : [],
             $dotEnvLocalValues ? $dotEnvLocalValues : [],
         );
+        //putting .env values
+        foreach ($dotEnvVars as $dotEnvVar => $dotEnvValue) {
+            if (null !== getenv($dotEnvVar)) {
+                continue;
+            }
+            putenv($dotEnvVar, $dotEnvValue);
+        }
         $envVars = [];
-        foreach (array_merge($dotEnvVars, getenv()) as $envVarKey => $envVarValue) {
+        //converting keys
+        foreach (getenv() as $envVarKey => $envVarValue) {
             $envVars[$this->sanitizeKey($envVarKey)] = $envVarValue;
         }
         return $envVars;
